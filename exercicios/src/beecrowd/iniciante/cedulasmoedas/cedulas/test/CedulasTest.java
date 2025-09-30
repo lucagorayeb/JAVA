15d738d3d50d77c485ba0e9e4c9bb555ea93561b
lucagorayeb <lucagorayeb@gmail.com>
2025-09-30 17:12:07 -0400

    doing some exercises

---
+++ exercicios/src/beecrowd/iniciante/cedulasmoedas/cedulas/test/CedulasTest.java
@@ -0,0 +1,9 @@
+package beecrowd.iniciante.cedulas.test;
+import beecrowd.iniciante.cedulas.domain.Cedulas;
+
+public class CedulasTest{
+	public static void main(String[] args){
+		Cedulas cedulas = new Cedulas();
+		cedulas.init(576);
+	}
+}
