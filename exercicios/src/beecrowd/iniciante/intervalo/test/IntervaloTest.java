463bd0a4320202e8c497a4578b54efa19b9ffad9
Luca Gorayeb <lucagorayeb@gmail.com>
2025-10-09 13:08:13 -0400

    doing some exercises

---
+++ exercicios/src/beecrowd/iniciante/intervalo/test/IntervaloTest.java
@@ -0,0 +1,9 @@
+package beecrowd.iniciante.intervalo.test;
+import beecrowd.iniciante.intervalo.domain.Intervalo;
+
+public class IntervaloTest{
+	public static void main(String[] args){
+		Intervalo intervalo = new Intervalo();
+		intervalo.verificaIntervalo(25.01);
+	}
+}
