e86e5836d0b80b415ed6cbbc22659fb1ee9116d2
luca <luca@localhost.localdomain>
2025-10-15 17:22:45 -0400

    doing some exercises

---
+++ exercicios/src/beecrowd/iniciante/sort_simples/domain/Sort.java
@@ -0,0 +1,16 @@
+package beecrowd.iniciante.sort_simples.domain;
+
+public class Sort{
+
+	public int[] ordenaValores(int[] array){
+		int maximo = array.length;
+		int minimo = 1;
+		int meio = maximo - minimo;
+
+		for(int i = minimo; i < meio; i++){
+			for(int j = 0; j < i; j++){
+				
+			}
+		}
+	}
+}
