700fd07b65eb61700c984c8e3255dfde571dc61c
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2025-11-03 08:08:27 -0400

    I forgot to commit earlier

--- exercicios/beecrowd/iniciante/tipostriangulos/src/TiposTrianguloTest.java
+++ exercicios/beecrowd/iniciante/tipostriangulos/src/TiposTrianguloTest.java
@@ -1,5 +1,7 @@
+import domain.TiposTriangulo;
 public class TiposTrianguloTest{
     public static void main(String[] args) {
-        
+        TiposTriangulo triangulo = new TiposTriangulo();
+        triangulo.mostrarSaida(7.0, 5.0, 7.0);
     }
 }
